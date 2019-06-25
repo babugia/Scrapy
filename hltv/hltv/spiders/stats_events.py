38edea5a38e98c44011eea566ff0948ffa87d567
babugia <babugia@icloud.com>
2019-06-25 19:56:39 -0300

    date, prize and teams, doing event banner

--- hltv/hltv/spiders/stats_events.py
+++ hltv/hltv/spiders/stats_events.py
@@ -1,3 +1,4 @@
+from colorama import Fore, Back, Style
 import scrapy
 
 
@@ -9,12 +10,34 @@ class StatsEvents(scrapy.Spider):
     ]
 
     def parse(self, response):
-        for camp in response.css('td.name-col '):
-            yield {
-                'title': camp.css('img::attr(title)').get(),
-            }
+        for camp in response.css('td.name-col a::attr(href)'):
+            campURL= response.urljoin(camp.get())
+            yield scrapy.Request(campURL, callback=self.parse_camp)   
 
         next_page = response.css('div.pagination-component.pagination-bottom a.pagination-next::attr(href)').get()
         if next_page is not None:
             next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback=self.parse)    
+            yield scrapy.Request(next_page, callback=self.parse)  
+
+    def parse_camp(self, response):
+        def extract_with_css(query):
+            return response.css(query).get(default='').strip()
+
+        def get_dates(query):
+            dates = response.css(query)[0]
+            date = dates.css("span::text").getall()
+            fullDate = ""
+
+            for d in date: 
+                fullDate += d
+
+            return fullDate
+
+        yield {
+            'location': extract_with_css('span.text-ellipsis::text'),
+            'date': get_dates('td.eventdate'),
+            'prizepool':extract_with_css('td.prizepool.text-ellipsis::text'),
+            'teams':extract_with_css('td.teamsNumber::text'),
+        }
+
+    # // TODO Save event name and banner
